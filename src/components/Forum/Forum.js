6997c37597a92d28180ec36ca402a16e78c24588
hridyansh07 <anshkhatri7@gmail.com>
2022-04-10 19:57:28 +0530

    Added Answer section and notification for upvote

--- src/components/Forum/Forum.js
+++ src/components/Forum/Forum.js
@@ -5,6 +5,19 @@ import { storeIntoIpfs, retriveDataIpfs } from "../Ipfs/ipfs";
 import moment from 'moment';
 import { submitPost, upVote, getVoteCount } from './ethersUtils';
 import { LikeOutlined, LikeFilled } from '@ant-design/icons';
+import { notification } from 'antd';
+
+const openNotification = () => {
+  notification.open({
+    message: 'Congratulations !!',
+    description:
+      'Congratulations for the correct answer, your reward is on your way !!!',
+    className: 'custom-class',
+    style: {
+      width: 600,
+    },
+  });
+};
 const layout = {
     labelCol: {
       span: 4,
@@ -23,10 +36,11 @@ export default function Forum() {
   const [likes, setLikes] = useState(0);
   const [tokenId, setTokenId] = useState(0);
   const [action, setAction] = useState(null);
-
+  const [ansTokenId, setAnswerTokenId] = useState(0);
+  const [voteCount, setVoteCount] = useState(0);
   const [form] = Form.useForm();
   let counter
-  
+  const MAX_COUNT = 1;
   const onFinish = (values) => {
     console.log(values);
     storeIntoIpfs (values).then(function (response) {
@@ -42,14 +56,33 @@ export default function Forum() {
     
   };
 
+  const onFinishAnswer = (values) => {
+    console.log(values);
+    storeIntoIpfs (values).then(function (response) {
+      console.log(response);
+      submitPost(response).then(function (response) {
+        console.log("Token ID",response.id);
+        setAnswerTokenId(response.id);
+      })
+    })
+    .catch(function (error) {
+      console.log(error);
+    }); 
+    
+  };
+  
   const like = async () => {   
     counter = likes
     counter++
     setLikes(counter);
-    await upVote(tokenId, 1)
-    getVoteCount(tokenId).then(function (response) {
-        console.log("TotalVoteCount",response)
-    })
+    await upVote(ansTokenId, 1)
+    let voteCount  = await getVoteCount(ansTokenId)
+    console.log("TotalVoteCount",voteCount)
+    //setVoteCount(voteCount)
+    if( voteCount == MAX_COUNT) {
+        openNotification()
+        // Integrate SuperFuild, stream some money
+    }
     //setDislikes(counter--);
     setAction('liked');
   };
@@ -101,7 +134,26 @@ export default function Forum() {
         </Button>
       </Form.Item>
     </Form>
+    <Form {...layout} form={form} name="control-hooks" onFinish={onFinishAnswer}>
+      <Form.Item
+        name="Answer"
+        label="Answer"
+        rules={[
+          {
+            required: false,
+          },
+        ]}
+      >
+         <Input.TextArea />
+      </Form.Item>
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Post
+        </Button>
+      </Form.Item>
+    </Form>
         </p>
+        
       }
       datetime={
         <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
